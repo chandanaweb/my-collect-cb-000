6abfdcd1ec0ddbe044b61f020298eaa1e15c1ce9
chandana M <chandu11288@gmail.com>
2018-02-09 19:19:07 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-my_collect(arr)
+def my_collect(arr)
  counter = 0
   while counter <arr.length
     yield(arr[counter])
     counter += 1
-  end 
+  end
 end
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
