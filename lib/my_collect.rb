e771146826bcf8da7f6e80d7306749eb30704d45
chandana M <chandu11288@gmail.com>
2018-02-09 19:17:40 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+my_collect(arr)
+ counter = 0
+  while counter <arr.length
+    yield(arr[counter])
+    counter += 1
+  end 
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
